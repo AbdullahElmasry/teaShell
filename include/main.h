879eac19eecae4b1ebf8c1c16084f7ab4c81d6ca
abdullahelmasry <abdallhking3040@gmail.com>
2025-09-19 06:16:55 +0300

    basic functionalities added

--- include/main.h
+++ include/main.h
@@ -3,5 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> // unix standards
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,36 @@
 #include "../include/main.h"
 
-int main(void)
+/*  by default the os is looking for a main function and the first
+    argument will the counter and the second will store those arguments
+    to iterate through them later using the argument vector.
+*/
+
+/*
+./runner ls -l
+ac = 3
+
+av →   ┌───────────────┐
+       │ av[0] ──► "./runner\0"
+       │ av[1] ──► "ls\0"
+       │ av[2] ──► "-l\0"
+       │ av[3] ──► NULL
+       └───────────────┘ 
+*/
+
+int main(int ac, char **av) // ac -> argument counter,  
 {
-    printf("Hello tea");
+    (void)ac;  // tells compiler that this wont be used
+    int status;
+
+    // child process
+    if (fork() == 0){ // fork() creates new child process and return 0, and returns child PID > 0 
+        execvp(av[1], av + 1);
+    }
+
+    /*  wait() -> system call that make parent process wait till child
+        process finish and return anything like "return 0" or "exit(0)"
+        that will be stored in -> status */
+    wait(&status); 
+
     return EXIT_SUCCESS;
 }
