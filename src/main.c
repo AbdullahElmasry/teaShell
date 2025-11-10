619d9dead1404b87f1e2678bd21fb7bd72bc55ca
Abdullah <Abdallhking3040@gmail.com>
2025-11-10 06:50:43 +0200

    dev branch initial commit

--- src/main.c
+++ src/main.c
@@ -17,20 +17,55 @@ av →   ┌───────────────┐
        └───────────────┘ 
 */
 
+char *cell_read_line(void){
+    char *buf;
+    size_t bufsize;
+
+  buf = NULL; // getline will take care of it
+}
+
+
 int main(int ac, char **av) // ac -> argument counter,  
 {
-    (void)ac;  // tells compiler that this wont be used
-    int status;
 
-    // child process
-    if (fork() == 0){ // fork() creates new child process and return 0, and returns child PID > 0 
-        execvp(av[1], av + 1);
+    char *line;
+    // REPL
+    // READ - EVALUATE - PRINT/EXECUTE - LOOP
+
+    while (0xCE77){
+
+        // 1- get line
+        // int getline();
+        line = cell_read_line();
+
+        /* 2- get tokens gettoke
+                -> lexing > parsing EVALUATING
+
+
+            3- Exec 
+        */
     }
 
+
+
+    // (void)ac;  // tells compiler that this wont be used
+    // int status; 
+
+    // // child process returns 0 so only child will enter the if.
+    // if (fork() == 0){ // fork() creates new child process and return 0
+
+    //     execvp(av[1], av + 1);
+
+    //     // v for "vector" , p for "path"
+    //     // takes an array of arguments and uses PATH to find the executable.
+    //     // char **av = {"ls", "-la", NULL}
+
+    // }
+
     /*  wait() -> system call that make parent process wait till child
-        process finish and return anything like "return 0" or "exit(0)"
-        that will be stored in -> status */
-    wait(&status); 
+    //     process finish and return anything like "return 0" or "exit(0)"
+    //     that will be stored in -> status */
+    wait(&status);
 
     return EXIT_SUCCESS;
 }
