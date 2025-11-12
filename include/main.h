de98eeb4449e6369af1ac0ffd550222326296133
Abdullah <Abdallhking3040@gmail.com>
2025-11-12 20:12:14 +0200

    test header with windows >> failed

--- include/main.h
+++ include/main.h
@@ -5,5 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h> // unix standards
 
+// #include <sys/wait.h>   // <-- NEW: Required for wait()
+// #include <sys/types.h>  // <-- Recommended: Often required for pid_t
 
 #endif 
